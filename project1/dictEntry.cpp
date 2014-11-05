2c939c2fad3e1ff440c5a4ae4716f3ea640acf1b
rubinz <zevirc@gmail.com>
2014-11-05 17:15:41 -0500

    testing alias for commiting

--- project1/dictEntry.cpp
+++ project1/dictEntry.cpp
@@ -1,2 +1,81 @@
 //project 1
 //by Zevi Rubin
+
+
+# include <iostream>
+# include <string>
+# include <string.h>
+# include <fstream>
+# include <time.h>
+# include <math.h>
+# include <stdio.h>
+# include <queue>
+# include <sstream>
+# include <stdlib.h>
+# include <cmath>
+# include "dictEntry.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "got here" << endl;
+
+	return 0;
+}
+
+bool dictEntry::operator==(string s){
+	if (this->word.compare(s) != 0){
+		return false;
+	else
+		return true;
+}
+bool dictEntry::operator<(string s){
+	if (this->word.compare(s) < 0){
+		return true;
+	else
+		return false;
+}
+bool dictEntry::operator<=(string s){
+	if ((this->word.compare(s) <= 0){
+		return true;
+	else
+		return false;
+}
+bool dictEntry::operator>(string s){
+	if (this->word.compare(s) > 0){
+		return true;
+	else
+		return false;
+}
+bool dictEntry::operator>=(string s){
+	if (this->word.compare(s) >= 0){
+		return true;
+	else
+		return false;
+}
+
+
+dictEntry::dictEntry(string w, string d){
+
+
+}
+
+
+dictEntry::~dictEntry(){
+
+
+}
+
+
+string dictEntry::getDefinition(){
+
+
+}
+
+
+void dictEntry::updateDefinition(string def){
+
+
+}
+
--- project1/dictEntry.h
+++ project1/dictEntry.h
@@ -1,6 +1,20 @@
 //project 1
 //by Zevi Rubin
 
+# include <iostream>
+# include <string>
+# include <string.h>
+# include <fstream>
+# include <time.h>
+# include <math.h>
+# include <stdio.h>
+# include <queue>
+# include <sstream>
+# include <stdlib.h>
+# include <cmath>
+
+using namespace std;
+
 
 class dictEntry
 {
@@ -10,6 +24,12 @@ public:
 	string getDefinition();
 	void updateDefinition(string def);
 
+	bool operator==(string s);
+	bool operator<(string s);
+	bool operator<=(string s);
+	bool operator>(string s);
+	bool operator>=(string s);
+	
 private:
 	string word;
 	string definition;
